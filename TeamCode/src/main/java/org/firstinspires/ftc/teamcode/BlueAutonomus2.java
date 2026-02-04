dddcf53daf17d4f81543ae0fa40f195747f4648b
Vote4Washington <edwinpaxtor10@gmail.com>
2026-02-03 21:50:34 -0800

    2nd iteration of autonomous code for red and blue sides.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueAutonomus2.java
@@ -0,0 +1,190 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+// red alliance autonomous attempt - 5'6 guy
+
+@Autonomous
+public class BlueAutonomus2 extends OpMode {
+
+    enum State {
+        STRAFE_LEFT,
+        DRIVE_INTAKE,
+        STRAFE_RIGHT,
+        DRIVE,
+        ROTATE,
+        SHOOT,
+        RETURN_DRIVE,
+        DONE
+    }
+
+    long timeElapsed;
+
+    State state;
+
+    DcMotor leftFrontDrive, leftBottomDrive, rightFrontDrive, rightBottomDrive, innerTake, outerTake, shooter;
+
+
+
+    @Override
+    public void init(){
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "leftFrontDrive");
+        leftBottomDrive = hardwareMap.get(DcMotor.class, "leftBottomDrive");
+        rightBottomDrive = hardwareMap.get(DcMotor.class, "rightBottomDrive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFrontDrive");
+        innerTake = hardwareMap.get(DcMotor.class, "innerIntake");
+        outerTake = hardwareMap.get(DcMotor.class, "outerIntake");
+        shooter = hardwareMap.get(DcMotor.class, "shooter");
+
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBottomDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBottomDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBottomDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBottomDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+
+        state = State.STRAFE_LEFT;
+        timeElapsed = System.currentTimeMillis();
+
+    }
+
+    @Override
+    public void loop(){
+        switch (state){
+            case STRAFE_LEFT:
+                strafeLeft(0.5);
+                if(timePassed(500)){
+                    stopBot();
+                    strafeLeft(0);
+                    nextState(State.DRIVE_INTAKE);
+                }
+                break;
+            case DRIVE_INTAKE:
+                setDaDrive(0.5, 0.5);
+                intakeOn();
+                if(timePassed(1000)){
+                    intakeOff();
+                    stopBot();
+                    nextState(State.STRAFE_LEFT);
+                }
+                break;
+            case STRAFE_RIGHT:
+                strafeRight(0.5);
+                if(timePassed(1000)){
+                    stopBot();
+                    strafeRight(0);
+                    nextState(State.DRIVE);
+                }
+                break;
+            case DRIVE:
+                setDaDrive(0.5, 0.5);
+                if(timePassed(1000)){
+                    stopBot();
+                    nextState(State.ROTATE);
+                }
+                break;
+            case ROTATE:
+                counterclockwiseRotation(0.5);
+                if(timePassed(500)){
+                    counterclockwiseRotation(0);
+                    stopBot();
+                    nextState(State.SHOOT);
+                }
+                break;
+            case SHOOT:
+                shooter.setPower(1);
+                intakeOn();
+                if(timePassed(1500)){
+                    intakeOff();
+                    shooter.setPower(0);
+                    stopBot();
+                    nextState(State.RETURN_DRIVE);
+                }
+                break;
+            case RETURN_DRIVE:
+                setDaDrive(-0.5, -0.5);
+                if(timePassed(1000)){
+                    stopBot();
+                    nextState(State.DONE);
+                }
+                break;
+            case DONE:
+                stopBot();
+                break;
+
+
+        }
+
+
+    }
+
+    void setDaDrive(double leftPower, double rightPower){
+        leftFrontDrive.setPower(leftPower);
+        leftBottomDrive.setPower(leftPower);
+        rightFrontDrive.setPower(rightPower);
+        rightBottomDrive.setPower(rightPower);
+    }
+
+    void strafeRight(double power){
+        leftFrontDrive.setPower(power);
+        leftBottomDrive.setPower(-power);
+        rightFrontDrive.setPower(-power);
+        rightBottomDrive.setPower(power);
+    }
+
+    void strafeLeft(double power){
+        leftFrontDrive.setPower(-power);
+        leftBottomDrive.setPower(power);
+        rightFrontDrive.setPower(power);
+        rightBottomDrive.setPower(-power);
+    }
+
+    void clockwiseRotation(double power){
+        leftFrontDrive.setPower(power);
+        leftBottomDrive.setPower(power);
+        rightFrontDrive.setPower(-power);
+        rightBottomDrive.setPower(-power);
+    }
+
+    void counterclockwiseRotation(double power){
+        leftFrontDrive.setPower(-power);
+        leftBottomDrive.setPower(-power);
+        rightFrontDrive.setPower(power);
+        rightBottomDrive.setPower(power);
+    }
+
+    void intakeOn(){
+        innerTake.setPower(1);
+        outerTake.setPower(1);
+    }
+
+    void intakeOff(){
+        innerTake.setPower(0);
+        outerTake.setPower(0);
+    }
+
+    void reverseIntake(){
+        innerTake.setPower(-1);
+        outerTake.setPower(-1);
+
+    }
+
+    void stopBot(){
+        setDaDrive(0, 0);
+    }
+
+    void nextState(State next){
+        state = next;
+        timeElapsed = System.currentTimeMillis();
+    }
+
+    boolean timePassed(long ms){
+        return System.currentTimeMillis() - timeElapsed >= ms;
+    }
+}
